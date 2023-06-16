5176555e45a8da86596fb6b91808de1109715b88
eduardsosh01~ <eduardsosh@gmail.com>
2023-06-17 01:12:48 +0300

    Changes for basic models

--- autosite/app/models.py
+++ autosite/app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Car(models.Model):
+    Make = models.CharField(max_length=50)
+    Model = models.CharField(max_length=50)
+    Year = models.IntegerField()
+    Fuel = models.CharField(max_length=50)
+    Engine_cc = models.IntegerField()
+    Gearbox = models.CharField(max_length=50)
+    Color = models.CharField(max_length=50)
+    
+class Listing(models.Model):
+    Car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    Price = models.IntegerField()
+    Mileage = models.IntegerField()
+    Location = models.CharField(max_length=50)
+    Description = models.TextField()
+    Date = models.DateTimeField(auto_now_add=True)
+    Phone = models.CharField(max_length=50)
+    Email = models.CharField(max_length=50)
+    Name = models.CharField(max_length=50)
+    
+class Image(models.Model):
+    Listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='listing_images/')
+    
